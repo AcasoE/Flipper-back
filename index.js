a36aaa01bbc1a656816fe9d4a24c9e504664f803
AcasoE <118763474+AcasoE@users.noreply.github.com>
2023-06-21 19:16:18 +0200

    index

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ server.use('/users', userRoutes)
 server.use('/posts', postsRoutes)
 
 
-server.use("*", (req, res, next)=>{
-    return res.status(404).json("Route not found");
+server.use("/", (req,res)=>{
+    res.send("funcionando");
 });
 
 
@@ -45,10 +45,10 @@ server.use((err, req, res, next) => {
     return res.status(err.status || 500).json(err.message || "Error inesperado");
 });
 
-
-server.use("/", (req,res)=>{
-    res.send("funcionando");
+server.use("*", (req, res, next)=>{
+    return res.status(404).json("Route not found");
 });
+
 server.listen(port,()=>{
     console.log(`El servidor está disponible en http://localhost:${port}`);
 });
